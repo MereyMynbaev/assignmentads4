de07d118f468abbe45ad55ea695574bec178acd3
MereyMynbaev <154600199+MereyMynbaev@users.noreply.github.com>
2024-05-28 08:31:52 +0500

    Update Vertex.java

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,27 @@
-public class Vertex {
+import java.util.*;
+
+public class Vertex<V> {
+    private final V data;
+    private final Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacent(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacent() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
 }
