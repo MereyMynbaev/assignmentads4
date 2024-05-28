4a3a2f55603c2d4dc3d4094c2d1186e95ec79d34
MereyMynbaev <154600199+MereyMynbaev@users.noreply.github.com>
2024-05-28 08:32:07 +0500

    Update WeightedGraph.java

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,33 @@
-public class WeightedGraph {
+import java.util.*;
+
+public class WeightedGraph<V> {
+    private final Map<V, Vertex<V>> vertices = new HashMap<>();
+    private final boolean directed;
+
+    public WeightedGraph(boolean directed) {
+        this.directed = directed;
+    }
+
+    public void addVertex(V data) {
+        vertices.putIfAbsent(data, new Vertex<>(data));
+    }
+
+    public void addEdge(V sourceData, V destData, double weight) {
+        addVertex(sourceData);
+        addVertex(destData);
+        Vertex<V> source = vertices.get(sourceData);
+        Vertex<V> dest = vertices.get(destData);
+        source.addAdjacent(dest, weight);
+        if (!directed) {
+            dest.addAdjacent(source, weight);
+        }
+    }
+
+    public Vertex<V> getVertex(V data) {
+        return vertices.get(data);
+    }
+
+    public Collection<Vertex<V>> getAllVertices() {
+        return vertices.values();
+    }
 }
